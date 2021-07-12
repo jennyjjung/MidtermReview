e07d087b2ab36a09dd73fae19b6be033043f2dbb
katsubear <katsubear@DESKTOP-OHDJH7F>
2021-07-12 14:31:52 -0400

    Add base use case

---
+++ src/prog24178/fx/TipCalcController.java
@@ -0,0 +1,22 @@
+package prog24178.fx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author katsubear
+ */
+public class TipCalcController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/prog24178/fx/TipCalculator.java
@@ -0,0 +1,16 @@
+package prog24178.fx;
+
+/**
+ *
+ * @author katsubear
+ */
+public class TipCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
